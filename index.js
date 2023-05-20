32e470bc5c40d77da469d0190586ade629a22076
Antor-094 <mdantor556677889@gmail.com>
2023-05-20 14:04:34 +0600

    Single Data update operation done

--- index.js
+++ index.js
@@ -15,75 +15,98 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phero.l
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const CarToyCollection = client.db('carToyDB').collection('toysCollection');
+        const CarToyCollection = client.db('carToyDB').collection('toysCollection');
 
-    app.post('/allToys', async (req, res) => {
-        const newCar = req.body;
-        // console.log(newCar);
-        const result = await CarToyCollection.insertOne(newCar);
-        res.send(result);
-    })
+        app.post('/allToys', async (req, res) => {
+            const newCar = req.body;
+            // console.log(newCar);
+            const result = await CarToyCollection.insertOne(newCar);
+            res.send(result);
+        })
 
-    app.get('/allToys', async (req, res) => {
-        const cursor = CarToyCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/allToys', async (req, res) => {
+            const cursor = CarToyCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    app.get('/allToys/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-  
-        const result = await CarToyCollection.findOne(query)
-  
-        res.send(result)
-      })
-    app.get('/allToy', async(req, res) => {
-    //  console.log('working')
-        console.log(req.query.email)
-  
-        let query = {}
-        if (req.query?.email) {
-          query = {
-            email: req.query.email
-          }
-        }
-        const result = await CarToyCollection.find(query).toArray()
-  
-        res.send(result)
-      })
+        app.get('/allToys/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
 
-      
-      app.delete('/allToys/:id', async (req, res) => {
+            const result = await CarToyCollection.findOne(query)
 
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-        const result = await CarToyCollection.deleteOne(query)
-        res.send(result)
-  
-      })
+            res.send(result)
+        })
+        app.get('/allToy', async (req, res) => {
+            //  console.log('working')
+            console.log(req.query.email)
+
+            let query = {}
+            if (req.query?.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const result = await CarToyCollection.find(query).toArray()
+
+            res.send(result)
+        })
+
+        app.put('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            // const options = { upsert: true };
+            const options = {
+
+          // Include only the specific data
+          projection: { quantity: 1, price:1, description:1,_id:1},
+        };
+            const updatedToy = req.body;
+            console.log(updatedToy)
+
+            const car = {
+                $set: {
+                    quantity: updatedToy.quantity,
+                    price: updatedToy.price,
+                    description: updatedToy.description
+                }
+            }
+
+            const result = await CarToyCollection.updateOne(filter, car, options);
+            res.send(result);
+        })
+
+        app.delete('/allToys/:id', async (req, res) => {
+
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await CarToyCollection.deleteOne(query)
+            res.send(result)
+
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -97,7 +120,7 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Toy Universe is running')
 });
 
