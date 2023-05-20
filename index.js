1d874cad8e71da9f7776d5c4d34c04813d468f2c
Antor-094 <mdantor556677889@gmail.com>
2023-05-20 09:22:29 +0600

    showing my toys using users email operation done

--- index.js
+++ index.js
@@ -43,6 +43,21 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/allToys', async (req, res) => {
+
+        console.log(req.query.email)
+  
+        let query = {}
+        if (req.query?.email) {
+          query = {
+            email: req.query.email
+          }
+        }
+        const result = await CarToyCollection.find(query).toArray()
+  
+        res.send(result)
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
