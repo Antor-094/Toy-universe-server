75aeba51dbff1821ce17a41ac0aa83126ef4b7ef
Antor-094 <mdantor556677889@gmail.com>
2023-05-20 09:44:32 +0600

    Delete operation done

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ async function run() {
   
         res.send(result)
       })
+      app.delete('/allToys/:id', async (req, res) => {
+
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await CarToyCollection.deleteOne(query)
+        res.send(result)
+  
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
